72e63d2f9e4111ce61a7ec32fc137f82e0adb9b4
Sergey Chistov <s.chistov@globus-ltd.com>
2026-03-26 13:17:49 +0300

    vercel build

--- backend/src/db/setup.js
+++ backend/src/db/setup.js
@@ -7,7 +7,7 @@ function getPool() {
   if (!pool) {
     pool = new Pool({
       connectionString: process.env.DATABASE_URL,
-      ssl: process.env.NODE_ENV === 'production' ? { rejectUnauthorized: false } : false,
+      ssl: process.env.DATABASE_URL?.includes('neon.tech') ? { rejectUnauthorized: false } : false,
     });
   }
   return pool;
